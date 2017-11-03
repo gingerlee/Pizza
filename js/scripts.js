2ca507c00da73bd7bc078adb1b06d24acc144969
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-11-03 15:12:34 -0700

    corrects math logic for total to be more accurate

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,22 @@
 function Pizza(base, size) {
   this.base = base;
   this.size = size;
-  this.addOns = [];
+  this.addOns;
   this.price = 0;
 }
 
-Pizza.prototype.setPizzaAddOnsPrice = function () {
-  if (this.addOns) {
-    return this.addOns.length * 2; //will take number of toppings in array and multiply by 2
-  }
-}
 
 Pizza.prototype.setPizzaBasePrice = function() {
-  if (this.base === "cheese") {
+  if (this.base === "Cheese") {
     return 8;
   } else { return 10;
   }
 }
 
 Pizza.prototype.setPizzaSizePrice = function() {
-  if (this.size === "small") {
+  if (this.size === "Small") {
     return 0;
-  } else if (this.size === "medium") {
+  } else if (this.size === "Medium") {
     return 4;
   } else { return 6;
   }
@@ -47,21 +42,20 @@ $(function() {
       selectedAddOns.push($(this).val());
     });
     var customPizza = new Pizza(selectedBase, selectedSize, selectedAddOns);
-    debugger;
 
     var basePrice = customPizza.setPizzaBasePrice();
     var sizePrice = customPizza.setPizzaSizePrice();
-    var addOnsPrice = customPizza.setPizzaAddOnsPrice();
-    // var addOnsPrice = selectedAddOns.length * 2;
+    var addOnsPrice = selectedAddOns.length * 2;
     var total = basePrice + sizePrice + addOnsPrice;
     console.log(total);
 
 
+    debugger;
     $("#place-order-button").click(function() {
       $("#order-display").show();
       $("#pizza-base").text(customPizza.base);
       $("#pizza-size").text(customPizza.size);
-      $("#pizza-add-ons").text(customPizza.addOns + ", ");
+      $("#pizza-add-ons").append(selectedAddOns.toString(" " + " ,"));
       $("#pizza-price").text(total);
 
       resetSelects();
