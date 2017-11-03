d6f2b2d9257ec737ac1e793f3bfab31fc2b56c11
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-11-03 13:26:26 -0700

    add checkboxes to pizza add ons html

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 // Business Logic
-function Pizza(base, size, addOn) {
+function Pizza(base, size) {
   this.base = base;
   this.size = size;
-  this.addOn = addOn;
+  this.addOns = [];
   this.price = 0;
+}
+
+Pizza.prototype.setPizzaAddOnsPrice = function () {
+
 }
 
 Pizza.prototype.setPizzaBasePrice = function() {
@@ -22,14 +26,6 @@ Pizza.prototype.setPizzaSizePrice = function() {
   }
 }
 
-Pizza.prototype.setPizzaAddOnPrice = function () {
-  if (this.addOn === "extra-cheese") {
-    return 1;
-  } else if (this.addOn === "mushrooms") {
-    return 2
-  } else { return 3;
-  }
-}
 
 function resetSelects() {
   $("select#pizza-base-select").val("");
@@ -44,12 +40,14 @@ $(function() {
 
     var selectedBase = $("#pizza-base-select").val();
     var selectedSize = $("#pizza-size-select").val();
-    var selectedAddOn = $("#pizza-add-on-select").val();
+
+    var selectedAddOns = $("#pizza-add-on-select").val();
+
     var customPizza = new Pizza(selectedBase, selectedSize, selectedAddOn);
 
     var basePrice = customPizza.setPizzaBasePrice();
     var sizePrice = customPizza.setPizzaSizePrice();
-    var addOnPrice = customPizza.setPizzaAddOnPrice();
+    var addOnPrice = customPizza.setPizzaAddOnsPrice();
     var total = basePrice + sizePrice + addOnPrice;
     console.log(total);
 
