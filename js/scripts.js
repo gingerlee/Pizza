8d8eba5136fba3c468ccad6bf6621d43b16d31b2
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-11-03 11:24:35 -0700

    add pizza price prototype with first branchingclear

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 // Business Logic
-function Pizza(base, size, addOn, price)
+function Pizza(base, size, addOn)
 this.base: base;
 this.size: size;
 this.addOn: addOn;
-this.price:
+this.price = 0;
 }
 
 Pizza.prototype.setPizzaPrice = function() {
-  
+  if (this.base === "cheese") {
+    return 8;
+  } else if (this.base === "pepperoni" || this.base === "pesto") {
+    return 10;
+  }
 }
